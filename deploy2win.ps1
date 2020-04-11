abd06adce7ed02d77b0be26bcd0591a5638f55c7
guillaume charbonnier <gcharbonnier>
2020-04-11 20:05:51 +0200

    change ci : linux & windows

--- deploy2win.ps1
+++ deploy2win.ps1
@@ -4,22 +4,22 @@
 param ($qtpath='C:\Qt\5.14.2')
 
 
-@echo ******set the proper env for windeploy to fetch the right vcredist
+echo ******set the proper env for windeploy to fetch the right vcredist
 #call "C:\Program Files (x86)\Microsoft Visual Studio\VS2017CommunityEdition\VC\Auxiliary\Build\vcvars64.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-@echo ******erase former deployement
+#call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+echo ******erase former deployement
 rmdir -r -fo .\deploy\windows\bin\
 
-@echo ******copy the newly build
+echo ******copy the newly build
 copy .\build\swag.exe .\deploy\windows\bin\
 
-@echo ******call windeploy
+echo ******call windeploy
 $qtpath\msvc2017_64\bin\windeployqt .\deploy\windows\bin\swag.exe -release -qmlimport=$qtpath\msvc2017_64\qml -qmldir=".\"
 
-@echo ******copy swag needed stuff
+echo ******copy swag needed stuff
 robocopy .\deps .\deploy\windows\deps /MIR
 robocopy .\src .\deploy\windows\src /MIR
 robocopy .\examples .\deploy\windows\examples /MIR
 
-@echo ******create new zip
+echo ******create new zip
 Compress-Archive -path .\deploy\windows -destinationpath .\swag_windows_v0.0.1.zip -compressionlevel optimal

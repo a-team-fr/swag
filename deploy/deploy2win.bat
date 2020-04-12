ec69552e57388d94d3389be789e0170a430a7f42
gcharbonnier <gcharbonnier@a-team.fr>
2020-04-12 12:54:03 +0200

    change in windows ci

--- deploy/deploy2win.bat
+++ deploy/deploy2win.bat
@@ -5,8 +5,9 @@ param ($qtpath='C:\Qt\5.14.2')
 
 
 echo ******set the proper env for windeploy to fetch the right vcredist
-#call "C:\Program Files (x86)\Microsoft Visual Studio\VS2017CommunityEdition\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\VS2017CommunityEdition\VC\Auxiliary\Build\vcvars64.bat" x64
 #call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 echo ******erase former deployement
 rmdir -r -fo .\deploy\windows\bin\
 
@@ -14,14 +15,12 @@ echo ******copy the newly build
 copy .\build\swag.exe .\deploy\windows\bin\
 
 echo ******call windeploy
-$qtpath\msvc2017_64\bin\windeployqt .\deploy\windows\bin\swag.exe -release -qmlimport=$qtpath\msvc2017_64\qml -qmldir=".\"
+"${qtpath}\msvc2017_64\bin\windeployqt" .\deploy\windows\bin\swag.exe -release -qmlimport=$qtpath\msvc2017_64\qml -qmldir=".\"
 
 echo ******copy swag needed stuff
-robocopy .\deps .\deploy\windows\deps /MIR
-robocopy .\src .\deploy\windows\src /MIR
-robocopy .\examples .\deploy\windows\examples /MIR
-
-VERSION=$(cat ./Version.def); export VERSION
+@rem robocopy .\deps .\deploy\windows\deps /MIR
+@rem robocopy .\src .\deploy\windows\src /MIR
+@rem robocopy .\examples .\deploy\windows\examples /MIR
 
 echo ******create new zip
 Compress-Archive -path .\deploy\windows -destinationpath .\swag.zip -compressionlevel optimal

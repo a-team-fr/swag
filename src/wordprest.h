374b2c48525bb4854c8c259643a216cd9d300d1f
guillaume charbonnier <gcharbonnier>
2020-05-14 11:05:39 +0200

    add missing licensing header

--- src/wordprest.h
+++ src/wordprest.h
@@ -1,3 +1,24 @@
+/****************************************************************************
+**
+** Copyright (C) 2020 A-Team.
+** Contact: https://a-team.fr/
+**
+** This file is part of the SwagSoftware free project.
+**
+**  SwagSoftware is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  SwagSoftware is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with SwagSoftware.  If not, see <https://www.gnu.org/licenses/>.
+**
+****************************************************************************/
 #ifndef WORDPREST_H
 #define WORDPREST_H
 
